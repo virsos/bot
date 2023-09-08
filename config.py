710e8f18402a1d7e3b22958935f156dae05ebbfc
virus <123871565+virsos@users.noreply.github.com>
2023-09-08 18:21:26 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "6058774175:AAGZSP1R__rNvSeOAHoYrV6CIYbfEIF1db0")
-SUDO = int(os.environ.get("SUDO", "5012406813"))
+SUDO = int(os.environ.get("SUDO", "6610195550"))
 HEROKU = os.environ.get("HEROKU", "jeiekeoek")
 APP_URL = "https://"+ HEROKU +".herokuapp.com/" + BOT_TOKEN
